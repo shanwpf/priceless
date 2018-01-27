28355e2753417e9eda1c0512aa8856aace1d52b2
shanwpf <shanwpf@gmail.com>
2018-01-27 16:49:20 +0800

    Add filter module and update project structure

--- modules/sources/carousell.py
+++ modules/sources/carousell.py
@@ -7,6 +7,11 @@ REPLACE_SPACE_STRING = "%20"
 def generate_url(search_str):
 	return URL_PREFIX + search_str.replace(" ", REPLACE_SPACE_STRING)
 
+# Carousell uses commas to separate thousands, eg. S$1,200.50
+# This function returns a string that is convertible to float
+def format_price(price_str):
+	return price_str[2:].replace(",", "")
+
 def create_item_list(html_doc):
 	item_list = []
 	soup = BeautifulSoup(html_doc.text, 'html.parser')
@@ -15,7 +20,7 @@ def create_item_list(html_doc):
 		item['username'] = card.find("h3", "media-heading").text
 		item['product_name'] = card.find("h4").text
 		item['date'] = card.find("time").find("span").text
-		item['price'] = card.find("dl").find("dd").text
+		item['price'] = format_price(card.find("dl").find("dd").text)
 		item['desc'] = card.select_one("dl dd:nth-of-type(2)").text
 		item_list.append(item)
 	return item_list
---
+++ modules/util/filterItems.py
@@ -0,0 +1,27 @@
+ITEM_NAME_FILTER_LIST = ['buying', 'wtb', 'buy', 'looking for']
+
+def is_bad_price(item):
+	item_price_float = float(item['price'])
+	if item_price_float < 1:
+		return True
+	else:
+		return False
+
+def is_bad_name(item):
+	item_name = item['product_name']
+	for phrase in ITEM_NAME_FILTER_LIST:
+		if phrase in item_name:
+			return True
+	return False
+
+def is_bad_item(item):
+	if is_bad_price(item) or is_bad_name(item):
+		return True
+	else:
+		return False
+
+def get_filtered_list(item_list):
+	for item in item_list:
+		if is_bad_item(item):
+			item_list.remove(item)
+	return item_list
