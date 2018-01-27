bac04b61cef0848201f14e6a245745a36658dc1d
shanwpf <shanwpf@gmail.com>
2018-01-27 16:50:02 +0800

    Filter item lists

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from modules import carousell
+from modules.sources import carousell
+from modules.util import filterItems
 
 print("Search: ", end = "")
 search_str = input()
 
-carousell_item_list = carousell.get_item_list(search_str)
+carousell_item_list = filterItems.get_filtered_list(carousell.get_item_list(search_str))
 print(carousell_item_list)
