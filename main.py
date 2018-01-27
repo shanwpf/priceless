9d508dad14b7e4cba8c4628185fba3ce920974c6
shanwpf <shanwpf@gmail.com>
2018-01-27 15:46:54 +0800

    Implement main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-# Main program goes here
+from modules import carousell
+
+print("Search: ", end = "")
+search_str = input()
+
+carousell_item_list = carousell.get_item_list(search_str)
+print(carousell_item_list)
