4289a9402eab56289f1aa849de668f6197906afe
shanwpf <shanwpf@gmail.com>
2018-01-27 20:21:19 +0800

    Rename scrape module to scraper

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from modules.util import scrape, curator
+from modules.util import scraper, curator
 
 print("Search: ", end = "")
 search_str = input()
 
-item_lists = scrape.get_item_lists(search_str)
+item_lists = scraper.get_item_lists(search_str)
 print(curator.get_curated_item_list(item_lists))
