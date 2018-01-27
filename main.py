d3469dd8ea894397e291a35ba16cc9f8d1a6485a
md-azsa <md.azsa@gmail.com>
2018-01-27 19:49:59 +0800

    Updated the sources and added fitlion into the list

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from modules.sources import carousell, lazada, qoo10
+from modules.sources import carousell, lazada, qoo10, fitlion
 from modules.util import filterItems
 
 print("Search: ", end = "")
@@ -7,4 +7,7 @@ search_str = input()
 carousell_item_list = filterItems.get_filtered_list(carousell.get_item_list(search_str))
 lazada_item_list = lazada.get_item_list(search_str)
 qoo10_item_list = qoo10.get_item_list(search_str)
-print(qoo10_item_list)
+fitlion_item_list = fitlion.get_item_list(search_str)
+print(fitlion_item_list)
+print('\n')
+print(carousell_item_list)
