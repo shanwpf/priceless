2f222002617eae4620838eb2937b972d36d4a66e
Zhu Lingjie <zhulingjiee@gmail.com>
2018-01-27 16:15:14 +0800

    Added lazada.py

---
+++ modules/lazada.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+URL_PREFIX = "https://www.lazada.sg/catalog/?q="
+REPLACE_SPACE_STRING = "+"
+
+def generate_url(search_str):
+	return URL_PREFIX + search_str.replace(" ", REPLACE_SPACE_STRING)
+
+def create_item_list(html_doc):
+	item_list = []
+	soup = BeautifulSoup(html_doc.text, 'html.parser')
+	cards = json.loads(soup.find_all("script", type="application/ld+json")[1].text)
+	for card in cards["itemListElement"]:
+		item = {}
+		item['product_name'] = card['name']
+		item['price'] = card['offers']['price']
+		item['url'] = card['url']
+		item_list.append(item)
+
+print json.dumps(item_list, indent=2, sort_keys=True) # Pretty prints
+
+def get_item_list(search_str):
+	html_doc = requests.get(generate_url(search_str))
+	return create_item_list(html_doc)
