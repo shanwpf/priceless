0aa6319719e54a30c6ebb847cf001be9a3959984
shanwpf <shanwpf@gmail.com>
2018-01-28 03:15:23 +0800

    Enable fitlion source

--- modules/util/scraper.py
+++ modules/util/scraper.py
@@ -1,4 +1,4 @@
-from modules.sources import carousell, lazada, qoo10, fitlion, gymshack
+from modules.sources import carousell, lazada, qoo10, fitlion, gymshack, ezbuy
 
 def get_item_lists(search_str):
 	item_lists = []
@@ -8,5 +8,6 @@ def get_item_lists(search_str):
 	item_lists.append(lazada.get_item_list(search_str))
 	item_lists.append(qoo10.get_item_list(search_str))
 	item_lists.append(gymshack.get_item_list(search_str))
-	#item_lists.append(fitlion.get_item_list(search_str))
+	item_lists.append(ezbuy.get_item_list(search_str))
+	item_lists.append(fitlion.get_item_list(search_str))
 	return item_lists
