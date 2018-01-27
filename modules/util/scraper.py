0d78ed6bb45dc187d16c2e6ff479e63f1395902b
shanwpf <shanwpf@gmail.com>
2018-01-28 02:09:13 +0800

    Include gymshack source and temporarily disable fitlion

--- modules/util/scraper.py
+++ modules/util/scraper.py
@@ -1,4 +1,4 @@
-from modules.sources import carousell, lazada, qoo10, fitlion
+from modules.sources import carousell, lazada, qoo10, fitlion, gymshack
 
 def get_item_lists(search_str):
 	item_lists = []
@@ -7,5 +7,6 @@ def get_item_lists(search_str):
 	item_lists.append(carousell.get_item_list(search_str))
 	item_lists.append(lazada.get_item_list(search_str))
 	item_lists.append(qoo10.get_item_list(search_str))
-	item_lists.append(fitlion.get_item_list(search_str))
+	item_lists.append(gymshack.get_item_list(search_str))
+	#item_lists.append(fitlion.get_item_list(search_str))
 	return item_lists
